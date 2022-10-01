8e48ec11908b337fdbc10082ea09820967ea5d5d
Casey Wilson <casey.j.wilson45@gmail.com>
2022-10-01 11:16:35 -0500

    Allow users to set sentivity for general method

--- Python/RamdomMain.py
+++ Python/RamdomMain.py
@@ -1,15 +1,12 @@
-from operator import truediv
 from random import randint
 import pafy
 import cv2
 import FileHandler
 from AnalysisClass import Analysis
-from PIL import ImageChops
-# from bs4 import BeautifulSoup
-from urllib.request import urlopen
 
 
 def getChoice():
+    """Prompts users to choose an options and returns that numerical value"""
     choice = -1
 
     while choice < 0:
@@ -28,6 +25,7 @@ def getChoice():
 
 
 def fetchCapture(url):
+    """Uses pafy to obtain the video from the URL. Then uses OpenCV to capture it."""
 
     video = pafy.new(url)
     best = video.getbest(preftype="mp4")
@@ -38,6 +36,8 @@ def fetchCapture(url):
 
 
 def jellyFishMethod():
+    """Actually performs the analysis of the video to create the package for the users.
+    It calls all neccesary functions."""
 
     image = 0
 
@@ -58,8 +58,7 @@ def jellyFishMethod():
         xWidth = frame.shape[0]
         yHeight = frame.shape[1]
 
-        count = 0
-        section = 0
+        count, section = 0, 0
 
         for x in range(0, xWidth, 2):
             # This splits each picture in to 9 sections
@@ -75,7 +74,7 @@ def jellyFishMethod():
                 r = frame[x, y, 2]
 
                 # If there's enough red (jellyfish!), then this logic is applied:
-                # 1) Create a decimal equivalent of the section we are in (8 = .8)
+                # 1) Create a decimal equivalent of the section we are in (example: 8 = .8)
                 # 2) Then add that value to how much red is in the pixel (32 / 255 == .125 / 10 = .0125)
                 # 3) So the resulting number is based on where the jelly is and how red it appears (.8125)
                 if r > 50:
@@ -96,12 +95,15 @@ def jellyFishMethod():
 
 
 def liveCamGeneral(url):
+    """A more general apporach to create random numbers from different videos. This simply compares
+    two seperate frames."""
 
     image = 0
 
     differenceAmount = .3
 
     frameCount = int(input("How many frames of the video should we analyze? "))
+    differenceAmount = getDesiredSensitivity()
 
     analyze = Analysis()
 
@@ -115,8 +117,7 @@ def liveCamGeneral(url):
 
     while success and image < frameCount:
 
-        count = 0
-        section = 0
+        count, section = 0, 0
 
         success, frame = capture.read()
 
@@ -141,8 +142,15 @@ def liveCamGeneral(url):
                 baseG = base[x, y, 1]
                 baseR = base[x, y, 2]
 
+                # These three if statements determine how much each pixel varies between two frames.
+                # If the variance is great enough, basically, the difference is saved.
                 if comapreTwoPixel(b, baseB) > differenceAmount:
+                    # Alter the pixel to denote difference was found
                     alter[x, y, 0] = 255
+                    # Create number by adding our section number to the % of the pixels b, g, or r value
+                    #Sample : .9       +   (220/255) /10
+                    # This would be .9  +   .0852
+                    # The result is .9852
                     n = (section / 10) + ((b / 255)/10)
                     analyze.addTrueRandomNumbers(n)
                     analyze.addPseudoRandomNumber(randint(0, 9))
@@ -169,7 +177,19 @@ def liveCamGeneral(url):
     analyze.formatAnalysisDocument()
 
 
+def getDesiredSensitivity():
+    differenceAmount = -1
+
+    while differenceAmount < 0 or differenceAmount > 1:
+        differenceAmount = float(input(
+            "Enter a decimal value to determine sensitivty: .1 = a little difference would create a number, .8 = a lot of difference would be need to create a number: "))
+
+    return differenceAmount
+
+
 def comapreTwoPixel(base, next):
+    """Returns the difference between two values (0-255). Because of how the number is 
+    stored, the value is halved."""
     difference = (base / 2) - (next / 2)
     return abs(difference) / (255 / 2)
 
@@ -189,4 +209,5 @@ def main():
         choice = getChoice()
 
 
-main()
+if __name__ == "__main__":
+    main()
